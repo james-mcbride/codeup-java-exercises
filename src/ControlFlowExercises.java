a76e3879490c8b897ac6ebf539d03015e71ee8a1
james McBride <james.m.mcbride15@gmail.com>
2021-01-22 17:04:45 -0600

    adding exercise number 2

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,15 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for (int i=5; i<=15; i++){
-            System.out.println(i);
-        }
-        for (long j =2; j<1000000; j*=j){
-            System.out.println(j);
+        for (int i=5; i<=100; i++) {
+            if (i%15==0){
+                System.out.println("fizzBuzz");
+            }else if (i%3==0) {
+                System.out.println("fizz");
+            } else if (i%5==0){
+                System.out.println("buzz");
+            } else {
+                System.out.println(i);
+            }
         }
     }
 }
