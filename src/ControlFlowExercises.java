eeb869b34d9d71eb50c5d4218fa27c305f25381f
james McBride <james.m.mcbride15@gmail.com>
2021-01-25 15:13:49 -0600

    added additional exercise to control statements and loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,15 +1,26 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for (int i=5; i<=100; i++) {
-            if (i%15==0){
-                System.out.println("fizzBuzz");
-            }else if (i%3==0) {
-                System.out.println("fizz");
-            } else if (i%5==0){
-                System.out.println("buzz");
-            } else {
-                System.out.println(i);
-            }
-        }
+//        for (int i=5; i<=100; i++) {
+//            if (i%15==0){
+//                System.out.println("fizzBuzz");
+//            }else if (i%3==0) {
+//                System.out.println("fizz");
+//            } else if (i%5==0){
+//                System.out.println("buzz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+//        Scanner scanner = new Scanner(System.in);
+//        int response = scanner.nextInt();
+//        System.out.println("number | squared | cubed");
+//        for (int i=0; i<=response; i++){
+//            System.out.println(i + "     | "+i*i+"     | "+ i*i*i);
+//        }
+
+
+
     }
 }
