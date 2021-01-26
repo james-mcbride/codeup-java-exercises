ed1600f0d6fee231c8c6b44594b99ba937802457
james McBride <james.m.mcbride15@gmail.com>
2021-01-26 15:15:34 -0600

    finished game development 101 exercise:

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
 public class MethodsExercises {
-    public static void add(int num1, int num2) {
-        System.out.println(num1+num2);
-//        return num1 + num2;
-    }
-    public static void subtract(int num1, int num2) {
-        System.out.println((num1 - num2));
-//        return num1 - num2;
-    }
+//    public static void add(int num1, int num2) {
+//        System.out.println(num1+num2);
+////        return num1 + num2;
+//    }
+//    public static void subtract(int num1, int num2) {
+//        System.out.println((num1 - num2));
+////        return num1 - num2;
+//    }
 //    public static void multiply(int num1, int num2) {
 //        int startingNum = num1;
 //        for (int i =1; i<num2; i++){
@@ -17,18 +17,29 @@ public class MethodsExercises {
 //        System.out.println(num1);
 ////        return num1 * num2;
 //    }
-public static int multiply(int num1, int num2) {
-    if (num2==0){
-        return 0;
-    } else if(num2==1){
-        return num1;
-    }
-    return num1+multiply(num1,(num2-1));
-//        return num1 * num2;
-}
-    public static void divide(int num1, int num2) {
-        System.out.println(num1 / num2);
-//        return num1/ num2;
+//public static int multiply(int num1, int num2) {
+//    if (num2==0){
+//        return 0;
+//    } else if(num2==1){
+//        return num1;
+//    }
+//    return num1+multiply(num1,(num2-1));
+////        return num1 * num2;
+//}
+//    public static void divide(int num1, int num2) {
+//        System.out.println(num1 / num2);
+////        return num1/ num2;
+//    }
+
+    public static long getInteger (long min, long max){
+        System.out.println("Enter a number between 1 and 10: ");
+        Scanner scanner = new Scanner(System.in);
+        long response = scanner.nextLong();
+        if (response<min || response>max){
+            return getInteger(min, max);
+        } else{
+            return response;
+        }
     }
 
     public static long factorial(long num){
@@ -38,11 +49,11 @@ public static int multiply(int num1, int num2) {
 //            result*=i;
 //        }
 //        return result;
-        if (num<1 || num>10){
-            System.out.println("Input a number between 1 and 10");
-//            long newInput =
-            return factorial(num);
-        }
+//        if (num<1 || num>10){
+//            System.out.println("Input a number between 1 and 10");
+////            long newInput =
+//            return factorial(num);
+//        }
         if (num==0){
             return 0;
         } else if (num ==1){
@@ -52,16 +63,59 @@ public static int multiply(int num1, int num2) {
     }
 
 
-    public static void main(String[] args) {
-        add(1,2);
-        subtract(1,2);
-        System.out.println(multiply(2, 6));
-        divide(6,3);
-        System.out.println("Input a number between 1 and 10");
+//    public static void main(String[] args) {
+//       long response = getInteger(1, 20);
+//      System.out.println(factorial(response));
+//    }
+    public static long diceSides(){
+        System.out.println("How many sides should your dice have?");
         Scanner scanner = new Scanner(System.in);
         long response = scanner.nextLong();
-        System.out.println(factorial(response));
+        return response;
     }
+    public static void rollDice(long diceSides){
+        long randomNumber1 = Math.round(Math.random()*diceSides);
+        long randomNumber2 = Math.round(Math.random()*diceSides);
+        System.out.println("You rolled " + randomNumber1 + " & " + randomNumber2);
+        System.out.println("Want to roll again?");
+        Scanner scanner = new Scanner(System.in);
+        String response2=scanner.nextLine();
+        if (response2.equalsIgnoreCase("yes") || response2.equalsIgnoreCase("y") ){
+             rollDice(diceSides());
+        } else{
+            System.out.println("Thanks for playing!");
+        }
+    }
+
+//    public static void main(String[] args) {
+//        rollDice(diceSides());
+//    }
+    public static long guessingGameUserInput(){
+        Scanner scanner = new Scanner(System.in);
+        long guess=scanner.nextLong();
+        return guess;
+    }
+    public static void guessingGameFeedback(long guess, long randomNumber){
+        if (guess<randomNumber){
+            System.out.println("HIGHER");
+            guessingGameFeedback(guessingGameUserInput(), randomNumber);
+        } else if (guess>randomNumber){
+            System.out.println("LOWER");
+            guessingGameFeedback(guessingGameUserInput(), randomNumber);
+        } else{
+            System.out.println("GOOD GUESS");
+        }
+    }
+    public static void guessingGame(){
+        System.out.println("We have picked a random Number between 1 and 100, take a guess what it is!");
+        long randomNumber =  Math.round(Math.random()*100);
+        guessingGameFeedback(guessingGameUserInput(), randomNumber);
+    }
+
+    public static void main(String[] args) {
+        guessingGame();
+    }
+
 
 
 }
